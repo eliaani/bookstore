dc6bdd78ab7735330d670124f1912655ed54dd73
Elias <eliashelin98@gmail.com>
2022-09-12 21:47:40 +0300

    Add book and delete book functionalities

--- src/main/java/teht/Bookstore/web/BookController.java
+++ src/main/java/teht/Bookstore/web/BookController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import teht.Bookstore.domain.Book;
 import teht.Bookstore.domain.BookRepository;
 
 @Controller
@@ -15,15 +17,27 @@ public class BookController {
 	@Autowired
 	BookRepository bookRepository;
 
-	@RequestMapping("index")
-	@ResponseBody
-	public String showIndex() {
-		return "This is the main page";
-	}
-	
 	@GetMapping("booklist")
 	public String showBooks(Model model) {
 		model.addAttribute("Books", bookRepository.findAll());
 		return "booklist";
 	}
+	
+	@GetMapping("add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    
+    @PostMapping("save")
+    public String save(Book book){
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+    	bookRepository.deleteById(id);
+        return "redirect:../booklist";
+    }   
 }
